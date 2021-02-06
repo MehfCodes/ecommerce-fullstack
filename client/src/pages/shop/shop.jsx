b7f6214bd09de7aad829a783be1b144bfbf2a9e8
mehfCodes <mrmohammadehf@gmail.com>
2021-02-06 18:35:59 +0330

    shop page, collection preview component and collection item component updated

---
+++ client/src/pages/shop/shop.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import CollectionPreview from '../../components/collection-preview/collectionPreview';
+import SHOP_DATA from './shop.data';
+
+export default class Shop extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollection }) => (
+          <CollectionPreview key={id} {...otherCollection} />
+        ))}
+      </div>
+    );
+  }
+}
