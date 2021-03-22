449111cae365bfe35061a7f6df3e5633b44b351e
MehfCodes <mrmohammadehf@gmail.com>
2021-03-23 00:28:30 +0430

    errors handled in login and sign component with animations,add ErrorMessage and AnimatedErrorMessage component

--- client/src/components/login/login.jsx
+++ client/src/components/login/login.jsx
@@ -7,6 +7,8 @@ import { Login as SignIn } from './../../utils/auth';
 import { setCurrentUser } from '../../redux/user/user.actions';
 import { connect } from 'react-redux';
 import { Spring } from 'react-spring/renderprops';
+
+import AnimatedErrorMessage from '../animated-error-message/animated-error-message';
 class Login extends Component {
   constructor(props) {
     super(props);
@@ -23,7 +25,6 @@ class Login extends Component {
       this.props.isLogin(data.token);
       this.props.history.push('/');
     } else {
-      console.log(error);
       this.setState({ error });
     }
   };
@@ -34,46 +35,60 @@ class Login extends Component {
       return st;
     });
   };
+
+  errorHandler = () => {
+    this.setState({ error: null });
+  };
   render() {
     return (
-      <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
-        {(props) => (
-          <div className="login" style={props}>
-            <h2>Sign in with your email and password</h2>
-            {/* <span>Sign in with your email and password</span> */}
-            <form onSubmit={this.handleSubmit} autoComplete="off">
-              <FormInput
-                handleChange={this.handleChange}
-                label="Username or Phone Number"
-                name="field"
-                type="field"
-                value={this.state.user.field}
-                // required
-                error={this.error ? true : null}
-              />
+      <>
+        <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
+          {(props) => (
+            <div className="login" style={props}>
+              <h2>Sign in with your email and password</h2>
+              {/* <span>Sign in with your email and password</span> */}
+              <form onSubmit={this.handleSubmit} autoComplete="off">
+                <FormInput
+                  handleChange={this.handleChange}
+                  label="Username or Phone Number"
+                  name="field"
+                  type="field"
+                  value={this.state.user.field}
+                  // required
+                  error={this.error ? true : null}
+                />
 
-              <FormInput
-                handleChange={this.handleChange}
-                label="Password"
-                name="password"
-                type="password"
-                value={this.state.user.password}
-                // required
-                error={this.error ? true : null}
-              />
-              <div className="buttons">
-                <Button type="submit">Login</Button>
-                <div className="suggest">
-                  Do not have an account?
-                  <span onClick={() => this.props.showSignUpHandler()}>
-                    Sign Up
-                  </span>
+                <FormInput
+                  handleChange={this.handleChange}
+                  label="Password"
+                  name="password"
+                  type="password"
+                  value={this.state.user.password}
+                  // required
+                  error={this.error ? true : null}
+                />
+                <div className="buttons">
+                  <Button type="submit">Login</Button>
+                  <div className="suggest">
+                    Do not have an account?
+                    <span onClick={() => this.props.showSignUpHandler()}>
+                      Sign Up
+                    </span>
+                  </div>
                 </div>
-              </div>
-            </form>
-          </div>
-        )}
-      </Spring>
+              </form>
+            </div>
+          )}
+        </Spring>
+        <AnimatedErrorMessage
+          items={this.state.error}
+          from={{ transform: 'translate3d(0,-120px,0)', opacity: 0 }}
+          enter={{ transform: 'translate3d(0,0px,0)', opacity: 1 }}
+          leave={{ transform: 'translate3d(0,-80px,0)', opacity: 0.5 }}
+          config={{ duration: 800 }}
+          errorHandler={this.errorHandler}
+        />
+      </>
     );
   }
 }
--- client/src/components/sign-up/sign-up.jsx
+++ client/src/components/sign-up/sign-up.jsx
@@ -6,82 +6,120 @@ import { signUp } from '../../utils/auth';
 import { withRouter } from 'react-router';
 import { connect } from 'react-redux';
 import { setCurrentUser } from '../../redux/user/user.actions';
+import AnimatedErrorMessage from '../animated-error-message/animated-error-message';
 
 class SignUp extends Component {
   constructor() {
     super();
     this.state = {
-      username: '',
-      phoneNumber: '',
-      password: '',
-      confirmPassword: '',
+      user: {
+        username: '',
+        phoneNumber: '',
+        password: '',
+        confirmPassword: '',
+      },
+      error: null,
     };
   }
   handleSubmit = async (event) => {
     event.preventDefault();
-    const { data, error } = await signUp(this.state);
+    const { data, error } = await signUp(this.state.user);
     if (!error) {
       this.props.setCurrentUser(data.token);
       this.props.history.push('/');
     } else {
-      console.log(error);
+      this.setState({ error });
     }
   };
+
   handleChange = (event) => {
     const { value, name } = event.target;
-    this.setState({ [name]: value });
+    this.setState((st) => {
+      st.user[name] = value;
+      return st;
+    });
   };
+
+  errorHandler = (i) => {
+    this.setState((st) => {
+      st.error.splice(i, 1);
+      return st;
+    });
+  };
+
   render() {
     return (
-      <div className="sign-up">
-        <h2 className="title">Sign up with your email and password</h2>
-        {/* <span>Sign up with your email and password</span> */}
-        <form
-          onSubmit={this.handleSubmit}
-          className="sign-up-form"
-          autoComplete="off"
-        >
-          <FormInput
-            type="text"
-            name="username"
-            value={this.state.username}
-            onChange={this.handleChange}
-            label="Username"
-            require="true"
-          ></FormInput>
-          <FormInput
-            type="phoneNumber"
-            name="phoneNumber"
-            value={this.state.phoneNumber}
-            onChange={this.handleChange}
-            label="Phone Number"
-            require="true"
-          ></FormInput>
-          <FormInput
-            type="password"
-            name="password"
-            value={this.state.password}
-            onChange={this.handleChange}
-            label="Password"
-            require="true"
-          ></FormInput>
-          <FormInput
-            type="password"
-            name="confirmPassword"
-            value={this.state.confirmPassword}
-            onChange={this.handleChange}
-            label="Confirm Password"
-            require="true"
-          ></FormInput>
-          <div className="buttons">
-            <Button type="submit">SIGN UP</Button>
-            <div className="suggest">
-              Do you have an account?{' '}
-              <span onClick={() => this.props.showSignUpHandler()}>Login</span>
+      <>
+        <div className="sign-up">
+          <h2 className="title">Sign up with your email and password</h2>
+          {/* <span>Sign up with your email and password</span> */}
+          <form
+            onSubmit={this.handleSubmit}
+            className="sign-up-form"
+            autoComplete="off"
+          >
+            <FormInput
+              type="text"
+              name="username"
+              value={this.state.user.username}
+              onChange={this.handleChange}
+              label="Username"
+              require="true"
+            ></FormInput>
+            <FormInput
+              type="phoneNumber"
+              name="phoneNumber"
+              value={this.state.user.phoneNumber}
+              onChange={this.handleChange}
+              label="Phone Number"
+              require="true"
+            ></FormInput>
+            <FormInput
+              type="password"
+              name="password"
+              value={this.state.user.password}
+              onChange={this.handleChange}
+              label="Password"
+              require="true"
+            ></FormInput>
+            <FormInput
+              type="password"
+              name="confirmPassword"
+              value={this.state.user.confirmPassword}
+              onChange={this.handleChange}
+              label="Confirm Password"
+              require="true"
+            ></FormInput>
+            <div className="buttons">
+              <Button type="submit">SIGN UP</Button>
+              <div className="suggest">
+                Do you have an account?{' '}
+                <span onClick={() => this.props.showSignUpHandler()}>
+                  Login
+                </span>
+              </div>
             </div>
-          </div>
-        </form>
-      </div>
+          </form>
+        </div>
+        <AnimatedErrorMessage
+          items={this.state.error}
+          from={{
+            transform: 'translate3d(0,-120px,0)',
+            opacity: 0,
+          }}
+          enter={{
+            transform: 'translate3d(0,0px,0)',
+            opacity: 1,
+          }}
+          leave={{
+            transform: 'translate3d(0,-80px,0)',
+            opacity: 0.5,
+          }}
+          config={{ duration: 300 }}
+          trail={300}
+          errorHandler={this.errorHandler}
+        />
+      </>
     );
   }
 }
